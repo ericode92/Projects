4ef433813ee31f5ef860ce0bf93f39d3d644bc9f
ericode92 <144123068+ericode92@users.noreply.github.com>
2025-04-18 09:00:30 +0900

    0418 work

--- My Dev/Ericreates Website/index.js
+++ My Dev/Ericreates Website/index.js
@@ -1,3 +1,20 @@
+// Nav Hover Active
+document.addEventListener("DOMContentLoaded", () => {
+  const current = window.location.pathname.split("/").pop(); // ex: about.html
+  const navLinks = document.querySelectorAll(".nav-lists a");
+
+  navLinks.forEach(link => {
+    const href = link.getAttribute("href");
+    const filename = href.split("/").pop(); // 비교 대상
+
+    if (filename === current || (filename === "index.html" && current === "")) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+// Search Function
 const data = ["Apple", "Banana", "Cherry", "Durian", "Orange", "Grape", "Watermelon"];
 
 const searchInput = document.getElementById("search-input");
